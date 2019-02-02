e531741f0c74985c465803b5043b9d3d311e421c
wdyoung <wesley@wesleyy.com>
2019-02-03 00:10:26 +1100

    Refactored greeter module to use irc handler class and throttling.

--- src/aussie_bot/modules/greeter.py
+++ src/aussie_bot/modules/greeter.py
@@ -1,12 +1,48 @@
-RESPONSES = {"o/", r"\o"}
+import logging
+import re
+from typing import Sequence, Tuple, Pattern
+
+from irc.client import ServerConnection, Event
+
+from aussie_bot.throttling import Throttle
+from . import IrcHandler
 
 
-def handler(connection, event):
-    if event.arguments and event.arguments[0] in RESPONSES:
-        response = {event.arguments[0]} ^ RESPONSES
-        if response:
-            connection.privmsg(event.target, list(response)[0])
+_LOGGER = logging.getLogger(__name__)
 
 
-def get_handlers():
-    return (("pubmsg", handler),)
+class Greeter(IrcHandler):
+    RESPONSES = (
+        (re.compile(r"^\.?o/"), r"\o"),
+        (re.compile(r"^(\\o\.?|hi|hello|bonjour|salut)"), "o/"),
+    )
+
+    def __init__(
+        self, responses: Sequence[Tuple[Pattern, str]] = None, throttle: Throttle = None
+    ):
+        super().__init__(throttle)
+
+        if responses is None:
+            responses = self.RESPONSES
+
+        self.responses = responses
+
+    def handler(self, connection: ServerConnection, event: Event):
+        if event.arguments:
+            occurrence_key = connection.server, event.target
+            if not self.throttle.is_throttled(*occurrence_key):
+                for pattern, response in self.responses:
+                    if pattern.match(event.arguments[0]):
+                        if not self.throttle.occurrence(*occurrence_key):
+                            if event.type == "privmsg":
+                                target = event.source.nick
+                            else:
+                                target = event.target
+                            connection.privmsg(target, response)
+                        break
+
+    def get_handlers(self):
+        return (("pubmsg", self.handler), ("privmsg", self.handler))
+
+
+get_handlers = Greeter()
